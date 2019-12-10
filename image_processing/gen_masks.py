8264e44d3fad706d7465dfcd0e5c4adff030e9e0
Ted Michael Celis <krazete@gmail.com>
2019-12-10 03:20:02 -0800

    updated gen_masks, changed ChOps method

--- image_processing/gen_masks.py
+++ image_processing/gen_masks.py
@@ -1,27 +1,49 @@
 import os
-from PIL import Image, ImageChops
-
-def iter_p(directory, show_error=False):
-    for filename in os.listdir(directory):
-        try:
-            yield Image.open(os.path.join(directory, filename)), filename
-        except Exception as message:
-            if show_error:
-                print('Error opening {}: {}.'.format(filename, message))
+from itertools import combinations
+from PIL import ImageChops
+from image_processing import file
 
 if __name__ == '__main__':
-    bigder = 'source/Art Capture'
-    for dirname in os.listdir(bigder):
-        directory = os.path.join(bigder, dirname)
+    file.mkdir('image_processing/output')
+    file.mkdir('image_processing/output/masks')
+    file.mkdir('image_processing/output/masks/raw')
+    file.mkdir('image_processing/output/masks/thin')
+
+    p = {}
+
+    capture = 'image_processing/input/Art Capture'
+    for character in os.listdir(capture):
+        p.setdefault(character, [])
+        directory = os.path.join(capture, character)
         if os.path.isdir(directory):
-            p = None
-            k = None
-            for im, filename in iter_p(directory):
+            # p = None
+            # k = None
+            for filename, im in file.iter_img(directory):
                 if '_PortraitMarquee_' in filename:
-                    if not p or not k:
-                        p = im
-                        k = im
-                    p = ImageChops.lighter(p, im)
-                    k = ImageChops.screen(k, im)
-            p.save('data/masks/raw/' + dirname + '.png')
-            k.save('data/masks/thin/' + dirname + '.png')
+                    p[character].append(im)
+            #         if not p or not k:
+            #             p = im
+            #             k = im
+            #         p = ImageChops.lighter(p, im)
+            #         k = ImageChops.screen(k, im)
+            # p.save('image_processing/output/masks/raw/' + character + '.png')
+            # k.save('image_processing/output/masks/thin/' + character + '.png')
+
+
+
+
+lor = None
+land = None
+for a, b in combinations(p['Peacock'], 2):
+    bina = i.convert('1', dither=False)
+    binb = j.convert('1', dither=False)
+    aorb = ImageChops.logical_or(bina, binb)
+    aandb = ImageChops.logical_and(bina, binb)
+    if not lor or not land:
+        lor = aorb
+        land = aandb
+    else:
+        lor = ImageChops.lighter(lor, aorb)
+        land = ImageChops.lighter(land, aandb)
+lor.save('image_processing/output/masks/raw/Peacock.png')
+land.save('image_processing/output/masks/raw/Peacock2.png')
