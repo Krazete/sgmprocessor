aa8a6e63f562bc03918ecb6538418496740c35e1
Ted Michael Celis <krazete@gmail.com>
2022-05-08 16:49:17 -0700

    shortened code

--- image_processing/gen_sprites.py
+++ image_processing/gen_sprites.py
@@ -2,26 +2,17 @@ import re
 import UnityPy
 from image_processing import file
 
-phone = UnityPy.load('image_processing/input/palettizedimages')
+if __name__ == '__main__':
+    phone = UnityPy.load('image_processing/input/palettizedimages')
 
-def all_assets(bundle):
-    for sf in phone.assets[bundle].values():
+    file.mkdir('image_processing/output')
+    file.mkdir('image_processing/output/sprite')
+
+    for sf in phone.assets['palettizedimages'].values():
         if isinstance(sf, UnityPy.files.SerializedFile):
             for k in sf.keys():
-                yield k, sf[k]
-
-def get_phonebook():
-    index = {}
-    for pid, obj in all_assets('palettizedimages'):
-        index[pid] = obj
-    return index
-
-phonebook = get_phonebook()
-
-for asset in all_assets('palettizedimages'):
-    if phonebook[asset[0]].type == "Texture2D":
-        var = phonebook[asset[0]]
-        tx = var.read()
-        im = UnityPy.export.Texture2DConverter.get_image_from_texture2d(tx)
-        fn = tx.read_type_tree()['name']
-        im.save('image_processing/output/sprite/' + fn + '.png')
+                if sf[k].type == 'Texture2D':
+                    md = sf[k].read()
+                    im = UnityPy.export.Texture2DConverter.get_image_from_texture2d(md)
+                    fn = md.read_type_tree()['name']
+                    im.save('image_processing/output/sprite/' + fn + '.png')
