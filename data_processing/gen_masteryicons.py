3856d9f680fbbe5ffc9703ce600e1eba6b9952fe
Ted Michael Celis <krazete@gmail.com>
2023-01-11 21:44:24 -0800

    Create gen_masteryicons.py

---
+++ data_processing/gen_masteryicons.py
@@ -0,0 +1,19 @@
+import UnityPy
+from data_processing import file
+
+file.mkdir('data_processing')
+file.mkdir('data_processing/output')
+file.mkdir('data_processing/output/images')
+
+apk = UnityPy.load('data_processing/input/base.apk')
+
+def extract_images(filter):
+    for asset in apk.assets:
+        for val in asset.values():
+            if val.type.name == 'Sprite' or val.type.name == 'Texture2D':
+                value = val.read()
+                if filter in value.name and hasattr(value, 'image'):
+                    value.image.convert('P').save('data_processing/output/images/{}.png'.format(value.name))
+
+if __name__ == '__main__':
+    extract_images('MasteryIcon')
