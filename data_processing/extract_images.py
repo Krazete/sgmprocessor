1559f1e469dd98c2278eb39b0d37dddb89d9779a
Ted Michael Celis <krazete@gmail.com>
2023-10-10 17:21:52 +0800

    skip exceptions

--- data_processing/extract_images.py
+++ data_processing/extract_images.py
@@ -12,24 +12,27 @@ def extract_images(query, directory='image', mode='P', flatten=True, first_only=
     saved = {}
     for asset in apk.assets:
         for val in asset.values():
-            if val.type.name == 'Sprite' or val.type.name == 'Texture2D':
-                value = val.read()
-                if query.lower() in value.name.lower() and hasattr(value, 'image'):
-                    img = value.image
-                    if mode:
-                        img = value.image.convert(mode)
-                    path = flatpath
-                    if not flatten:
-                        path += '/' + asset.name
-                        file.mkdir(path)
-                    if value.name in saved:
-                        saved[value.name] += 1
-                        img.save('{}/{} ({}).png'.format(path, value.name, saved[value.name]))
-                    else:
-                        saved[value.name] = 0
-                        img.save('{}/{}.png'.format(path, value.name))
-                        if first_only:
-                            return
+            try:
+                if val.type.name == 'Sprite' or val.type.name == 'Texture2D':
+                    value = val.read()
+                    if query.lower() in value.name.lower() and hasattr(value, 'image'):
+                        img = value.image
+                        if mode:
+                            img = value.image.convert(mode)
+                        path = flatpath
+                        if not flatten:
+                            path += '/' + asset.name
+                            file.mkdir(path)
+                        if value.name in saved:
+                            saved[value.name] += 1
+                            img.save('{}/{} ({}).png'.format(path, value.name, saved[value.name]))
+                        else:
+                            saved[value.name] = 0
+                            img.save('{}/{}.png'.format(path, value.name))
+                            if first_only:
+                                return
+            except:
+                continue
 
 if __name__ == '__main__':
     extract_images('MasteryIcon')
