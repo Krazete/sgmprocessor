1b94a577fbe922118db72fa832459be35a9b0cf6
Ted Michael Celis <krazete@gmail.com>
2025-12-18 14:59:41 -0800

    removed case 31; default consumable is likely inaccurate

--- data_processing/loot.py
+++ data_processing/loot.py
@@ -132,10 +132,6 @@ def mine_loot(loot, showID=False):
             gearptr = loot['gear']
             gear = sa0_get_id(gearptr)
             extra = '{}, {}'.format(gear['title'].split('_')[1], corpus['en'][gear['title']]) # todo: detect base guest star properly and differentiate variants
-        case 31:
-            itemptr = loot['consumable']
-            item = sa0_get_id(itemptr)
-            extra = corpus['en'][item['title']]
         case _:
             if lootType not in lootTypes:
                 from pprint import pprint
